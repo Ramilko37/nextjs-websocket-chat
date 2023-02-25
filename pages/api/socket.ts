5d555f0d9077352c65c18b33454641331472f608
Ramilko37 <galyamdin@gmail.com>
2023-02-25 21:14:48 +0300

    added multiple connection

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,12 +1,27 @@
 import { NextApiRequest } from "next";
 import NextApiResponseServerIO from "../../types/next";
-import { Server as ServerIO } from "socket.io";
+import { Server as ServerIO, Socket } from "socket.io";
 import { Server as NetServer } from "http";
 
 export const config = {
   api: {
     bodyParser: false,
   },
+};
+
+const handleConnection = (socket: Socket) => {
+  console.log("A user has connected!");
+
+  // handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected!");
+  });
+
+  // handle incoming message
+  socket.on("chat:message", (data) => {
+    console.log("New message:", data);
+    socket.broadcast.emit("chat:message", data);
+  });
 };
 
 export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
@@ -19,6 +34,11 @@ export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
     });
     // append SocketIO server to Next.js socket server response
     res.socket.server.io = io;
+    // handle new connections
+    io.on("connection", handleConnection);
+  } else {
+    // handle new connections
+    res.socket.server.io.on("connection", handleConnection);
   }
   res.end();
 };
